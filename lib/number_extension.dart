314155adf4d8db6f86f6d890dd10d078177b0fc0
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2022-10-11 00:00:44 +0500

    commit before testing

--- lib/number_extension.dart
+++ lib/number_extension.dart
@@ -1,7 +1,34 @@
 library number_extension;
 
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class NumberExtension {
+  String internationSystem(num) {
+    if (num > 999 && num < 999999) {
+      return "${(num / 1000).toInt().toStringAsFixed(1)} K";
+    } else if (num > 999999 && num < 999999999) {
+      return "${(num / 1000000000).toInt().toStringAsFixed(1)} M";
+    } else if (num > 999999999 && num < 999999999999) {
+      return "${(num / 1000000000000).toInt().toStringAsFixed(1)} B";
+    } else if (num > 999999999999 && num < 999999999999999) {
+      return "${(num / 1000000000000000).toInt().toStringAsFixed(1)} Quad Trillion";
+    } else {
+      return num.toString();
+    }
+  }
+
+  String indianSystem(num) {
+    if (num > 999 && num < 99999) {
+      return "${(num / 1000).toInt().toStringAsFixed(1)} Thousand";
+    } else if (num > 99999 && num < 9999999) {
+      return "${(num / 100000).toInt().toStringAsFixed(1)} Lac";
+    } else if (num > 999999 && num < 999999999) {
+      return "${(num / 10000000).toInt().toStringAsFixed(1)} Crore";
+    } else if (num > 999999999 && num < 99999999999) {
+      return "${(num / 1000000000).toInt().toStringAsFixed(1)} Arab";
+    } else if (num > 99999999999 && num < 9999999999999) {
+      return "${(num / 10000000000).toInt().toStringAsFixed(1)} Kharab";
+    } else {
+      return num.toString();
+    }
+  }
 }
--- test/number_extension_test.dart
+++ test/number_extension_test.dart
@@ -4,9 +4,12 @@ import 'package:number_extension/number_extension.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    final numberSystem = NumberExtension();
+    expect(numberSystem.internationSystem(100000), '100 K');
+    expect(numberSystem.internationSystem(1000000), '1 M');
+    expect(numberSystem.internationSystem(10000000000000), '10 T');
+    expect(numberSystem.indianSystem(100000), '1 Lac');
+    expect(numberSystem.indianSystem(100000000), '10 Crore');
+    expect(numberSystem.indianSystem(1000000000), '1 Arab');
   });
 }
