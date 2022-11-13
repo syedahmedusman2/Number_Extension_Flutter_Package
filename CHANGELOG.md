981897a6f1d580bd69a367224d0d2ca665b73361
Ahmed Usman <ahmedusmanpakistan@gmail.com>
2022-11-14 00:03:55 +0500

    add example app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
 * Number Extension for International and Indian counting system.
+
+## 0.0.2
+
+* Fix bugs from initial release
--- lib/number_extension.dart
+++ lib/number_extension.dart
@@ -3,13 +3,17 @@ library valueber_extension;
 //We could simply use "${(value / 1000).toStringAsFixed(1)} K" like this but for controlling zero in the result, we are using the below condition;
 class NumberExtension {
   static String internationSystem(value, {numbersAfterPoint = 1}) {
-    if (double.parse(value.toString()) > 999 && double.parse(value.toString()) < 999999) {
+    if (double.parse(value.toString()) > 999 &&
+        double.parse(value.toString()) < 999999) {
       return "${(double.parse(value.toString()) / 1000).toStringAsFixed((double.parse(value.toString()) / 1000).truncateToDouble() == (double.parse(value.toString()) / 1000) ? 0 : numbersAfterPoint)} K";
-    } else if (double.parse(value.toString()) > 999999 && double.parse(value.toString()) < 999999999) {
+    } else if (double.parse(value.toString()) > 999999 &&
+        double.parse(value.toString()) < 999999999) {
       return "${(double.parse(value.toString()) / 1000000).toStringAsFixed((double.parse(value.toString()) / 1000000).truncateToDouble() == (double.parse(value.toString()) / 1000000) ? 0 : numbersAfterPoint)} Million";
-    } else if (double.parse(value.toString()) > 999999999 && double.parse(value.toString()) < 999999999999) {
+    } else if (double.parse(value.toString()) > 999999999 &&
+        double.parse(value.toString()) < 999999999999) {
       return "${(double.parse(value.toString()) / 1000000000).toStringAsFixed((double.parse(value.toString()) / 1000000000).truncateToDouble() == (double.parse(value.toString()) / 1000000000) ? 0 : numbersAfterPoint)} Billion";
-    } else if (double.parse(value.toString()) > 999999999999 && double.parse(value.toString()) < 999999999999999) {
+    } else if (double.parse(value.toString()) > 999999999999 &&
+        double.parse(value.toString()) < 999999999999999) {
       return "${(double.parse(value.toString()) / 1000000000000).toStringAsFixed((double.parse(value.toString()) / 1000000000000).truncateToDouble() == (double.parse(value.toString()) / 1000000000000) ? 0 : numbersAfterPoint)} Trillion";
     } else if (double.parse(value.toString()) > 999999999999999) {
       return "${(double.parse(value.toString()) / 1000000000000000).toStringAsFixed((double.parse(value.toString()) / 1000000000000000).truncateToDouble() == (value / 1000000000000000) ? 0 : numbersAfterPoint)} Quad Trillion";
@@ -19,13 +23,17 @@ class NumberExtension {
   }
 
   static String indianSystem(value, {numbersAfterPoint = 1}) {
-    if (double.parse(value.toString()) > 999 && double.parse(value.toString()) < 99999) {
+    if (double.parse(value.toString()) > 999 &&
+        double.parse(value.toString()) < 99999) {
       return "${(double.parse(value.toString()) / 1000).toStringAsFixed((double.parse(value.toString()) / 1000).truncateToDouble() == (double.parse(value.toString()) / 1000) ? 0 : numbersAfterPoint)} Thousand";
-    } else if (double.parse(value.toString()) > 99999 && double.parse(value.toString()) < 9999999) {
+    } else if (double.parse(value.toString()) > 99999 &&
+        double.parse(value.toString()) < 9999999) {
       return "${(double.parse(value.toString()) / 100000).toStringAsFixed((double.parse(value.toString()) / 100000).truncateToDouble() == (double.parse(value.toString()) / 100000) ? 0 : numbersAfterPoint)} Lac";
-    } else if (double.parse(value.toString()) > 999999 && double.parse(value.toString()) < 999999999) {
+    } else if (double.parse(value.toString()) > 999999 &&
+        double.parse(value.toString()) < 999999999) {
       return "${(double.parse(value.toString()) / 10000000).toStringAsFixed((double.parse(value.toString()) / 10000000).truncateToDouble() == (double.parse(value.toString()) / 10000000) ? 0 : numbersAfterPoint)} Crore";
-    } else if (double.parse(value.toString()) > 999999999 && double.parse(value.toString()) < 99999999999) {
+    } else if (double.parse(value.toString()) > 999999999 &&
+        double.parse(value.toString()) < 99999999999) {
       return "${(double.parse(value.toString()) / 1000000000).toStringAsFixed((double.parse(value.toString()) / 1000000000).truncateToDouble() == (double.parse(value.toString()) / 1000000000) ? 0 : numbersAfterPoint)} Arab";
     } else if (double.parse(value.toString()) > 99999999999) {
       return "${(double.parse(value.toString()) / 100000000000).toStringAsFixed((double.parse(value.toString()) / 100000000000).truncateToDouble() == (double.parse(value.toString()) / 100000000000) ? 0 : numbersAfterPoint)} Kharab";
